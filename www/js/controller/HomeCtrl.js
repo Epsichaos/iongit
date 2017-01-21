5eaabf687147af1638e1d587ff49d5c242d6d04e
Epsichaos <constant.camille.chaos@gmail.com>
2017-01-21 04:11:17 +0100

    good night

--- www/js/controller/HomeCtrl.js
+++ www/js/controller/HomeCtrl.js
@@ -6,16 +6,13 @@ app.controller('HomeCtrl', function ($scope,
                                      DatabaseService) {
 
   $ionicPlatform.ready(function () {
+    
     /** --- INIT SCOPE FUNCTION --- **/
     $scope.init = function () {
       console.log('scope init');
       DatabaseService.createDB().then(function () {
         DatabaseService.createTable().then(function (success) {
           console.log("Database & table created");
-          /*
-          $scope.oauthResponse = DatabaseService.getUserToken();
-          console.log($scope.oauthResponse);
-          */
         }, function (error) {
           console.log(error);
         });
@@ -27,11 +24,10 @@ app.controller('HomeCtrl', function ($scope,
       console.log('github oauth');
       $cordovaOauth.github(APP_PUBLIC_KEY,
         APP_SECRET_KEY, [APP_EMAIL]).then(function (res) {
-        //console.log(JSON.stringify(res));
         $scope.oauthResponse = JSON.stringify(res);
-        DatabaseService.saveUserToken($scope.oauthResponse.access_token).then(function(success) {
+        DatabaseService.saveUserToken($scope.oauthResponse.access_token).then(function (success) {
           console.log(DatabaseService.getUserToken());
-        }, function(error) {
+        }, function (error) {
           console.log(error);
         });
       }, function (error) {
